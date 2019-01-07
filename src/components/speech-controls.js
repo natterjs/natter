0618ace33d2959b53528ba10d4acf1b17c054ed7
Simon <simonireilly@gmail.com>
2019-01-07 09:15:58 +0000

    At the semantic user interface

--- src/components/speech-controls.js
+++ src/components/speech-controls.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 const speech = require('../services/speech')
 
 export default class SpeechControls extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={speech.start}>Launch speech</button>
-        <button onClick={speech.stop}>Stop recording</button>
+        <Button onClick={speech.start}>Launch speech</Button>
+        <Button onClick={speech.stop}>Stop recording</Button>
       </div>
     );
   }
