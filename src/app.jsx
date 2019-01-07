06214dc0fe0893dad713739bd558c890026da49b
Simon <simonireilly@gmail.com>
2019-01-07 17:21:29 +0000

    Add a scratch pad for some in app testing of the recognition

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,11 @@
 // Libraries
 import React from 'react';
+import { Container } from 'semantic-ui-react'
+
 
 // Components
 import Navbar from './components/navigation/navbar.js'
+import ScratchPad from './components/elements/scratch-pad.js'
 
 // Configuration
 import config from './config/config'
@@ -12,8 +15,13 @@ import speech from './services/speech'
 
 export default class App extends React.Component {
   render() {
-    return (<div>
-      <Navbar speech={speech} />
-    </div>);
+    return (
+      <div>
+        <Navbar speech={speech} />
+        <Container>
+          <ScratchPad />
+        </Container>
+      </div>
+    );
   }
 }
