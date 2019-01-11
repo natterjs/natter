ce19b36c4c3ddd26b1fc49443799ab9dc65f427d
Simon <simonireilly@gmail.com>
2019-01-11 07:34:41 +0000

    Make the executors plugs and adapters

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 import { enableLiveReload } from 'electron-compile';
-import executeString from './services/executor/robot-js-executor.js'
+import executors from './services/executors'
 import broadcastParser from './services/parser/broadcast-parser'
 
 const ipcMain = require( 'electron').ipcMain;
@@ -10,7 +10,7 @@ ipcMain.on('speech-broadcast', function(event, data) {
   console.log(`Main Recieved ->`);
   console.log(data);
   let parsedString = broadcastParser(data);
-  executeString(data);
+  executors['robot-js']['string'](data);
 });
 
 // Keep a global reference of the window object, if you don't, the window will
