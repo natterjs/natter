b93c09e391c9ac7e8b2bc7acfde0b474358e9893
Simon <simonireilly@gmail.com>
2019-03-16 13:29:40 +0000

    Add pending tests

---
+++ test/services/speech/wit-ai-spec.js
@@ -0,0 +1,26 @@
+// Libraries
+const chai = require("chai");
+const expect = chai.expect;
+const sinon = require("sinon")
+
+// Services
+import robotJSExecutor from '../../../src/services/speech/wit-ai.js'
+
+describe('WIT AI Speech API', function () {
+  describe('.start', function () {
+    it('handles API success', function () {
+      console.log("PENDING")
+    })
+
+    it('handles API failure', function () {
+      console.log("PENDING")
+    })
+  })
+
+  describe('.stop', function () {
+    it('prevents further API calls', function () {
+      console.log("PENDING")
+
+    })
+  })
+})
