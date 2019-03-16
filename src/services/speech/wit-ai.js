1e8431376249e14f8ebe2c37949e6578c8621ef6
Simon <simonireilly@gmail.com>
2019-03-16 13:18:09 +0000

    Add proper toggling to WIT AI

--- src/services/speech/wit-ai.js
+++ src/services/speech/wit-ai.js
@@ -1,3 +1,7 @@
+// This module sends data to wit AI when the user speaks
+//
+// It exports it's required start and stop commands
+
 // Libraries
 const record = require('node-record-lpcm16')
 const request = require('request')
@@ -5,24 +9,13 @@ const request = require('request')
 // Configuration
 const config = require('../../config/config')
 
-// Create a recognize stream
-const recognizeStream = (callBack) => {
-  return (
-    request.post({
-        'url'     : 'https://api.wit.ai/speech?client=chromium&lang=en-us&output=json',
-        'headers' : {
-          'Accept': 'application/vnd.wit.20160202+json',
-          'Authorization': 'Bearer ' + config.witToken,
-          'Content-Type': 'audio/wav'
-        }
-      }, callBack)
-  )
-}
+// We relie on sox for audio streaming - this active will break it's loop
+// calls when we toggle it
+let active = false
 
-const startRecording = (processSpeech) => {
-
+const recorder = (processSpeech) => {
   const parseResult = function (err, resp, body) {
-    startRecording(processSpeech)
+    restartRecording(processSpeech)
     let data = JSON.parse(body)
     let message = {
       text: data["_text"],
@@ -37,11 +30,37 @@ const startRecording = (processSpeech) => {
     recordProgram: 'rec',
     silence: '0.5',
     threshold: 5
-  }).pipe(recognizeStream(parseResult))
+  }).pipe(postAudioData(parseResult))
+}
+
+const startRecording = (callBack) => {
+  active = true
+  recorder(callBack)
+}
+
+const restartRecording = (callBack) => {
+  if (active) {
+    recorder(callBack)
+  }
 }
 
 const stopRecording = () => {
+  active = false
   record.stop()
+}
+
+// Create a recognize stream
+const postAudioData = (callBack) => {
+  return (
+    request.post({
+        'url'     : 'https://api.wit.ai/speech?client=chromium&lang=en-us&output=json',
+        'headers' : {
+          'Accept': 'application/vnd.wit.20160202+json',
+          'Authorization': 'Bearer ' + config.witToken,
+          'Content-Type': 'audio/wav'
+        }
+      }, callBack)
+  )
 }
 
 module.exports.start = startRecording
