d3900b0d40713b6fe7d982ba50b213cbda990293
Simon <simonireilly@gmail.com>
2019-03-23 20:44:26 +0000

    Initial commit with new tests

---
+++ src/pages/settings.jsx
@@ -0,0 +1,12 @@
+// Libraries
+import React from 'react'
+
+export default class Settings extends React.Component {
+  render(){
+    return (
+      <div>
+        <p>Settings</p>
+      </div>
+    )
+  }
+}
---
+++ test/integration/integration-spec.js
@@ -0,0 +1,34 @@
+const testhelper = require("../support/spectron-helper");
+const app = testhelper.initialiseSpectron();
+
+const chaiAsPromised = require("chai-as-promised");
+const chai = require("chai");
+chai.should();
+chai.use(chaiAsPromised);
+
+describe("Example integrations spec", function () {
+  this.timeout(30000)
+
+  // Start spectron
+  before(function () {
+    chaiAsPromised.transferPromiseness = app.transferPromiseness;
+    return app.start();
+  });
+
+  // Stop Electron
+  after(function () {
+    if (app && app.isRunning()) {
+      return app.stop();
+    }
+  });
+
+  describe("start Up", function () {
+    // wait for Electron window to open
+    // When it is headless in CI it is 1
+    // When running locally it is 2
+    it('opens application window', function () {
+      return app.client.waitUntilWindowLoaded().getWindowCount()
+      .should.eventually.below(3);
+    });
+  });
+});
