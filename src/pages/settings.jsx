9c159e8c91615e78d15ae58a70884cb5336e7f93
Simon <simonireilly@gmail.com>
2019-05-06 08:21:27 +0100

    Fix indentation in settings.jsx

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,6 +1,6 @@
 // Libraries
 import React from 'react'
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 // Import pages
 import Configuration from './settings/configuration'
@@ -13,17 +13,17 @@ import SettingsMenu from '../components/settings/settings-menu'
 export default class Settings extends React.Component {
   render(){
     return (
-    <Router>
-      <div>
-      <SettingsMenu />
-        <Switch>
-          <Route exact path="/" component={Configuration} />
-          <Route path="/grammars" component={Grammars} />
-          <Route path="/keyboard" component={Keyboard} />
-          <Redirect to="/" />
-        </Switch>
-      </div>
-    </Router>
+      <Router>
+        <div>
+        <SettingsMenu />
+          <Switch>
+            <Route exact path="/" component={Configuration} />
+            <Route path="/grammars" component={Grammars} />
+            <Route path="/keyboard" component={Keyboard} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
